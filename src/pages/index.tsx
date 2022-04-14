d24c3df973bb0d34d590f57af14783cb58063197
Alex Pagnotta <ap.alex.pagnotta@gmail.com>
2022-04-14 21:06:42 +0200

    Initial work on basic UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
+import { styled } from '../../stitches.config'
+
 const Homepage: NextPage = () => {
   return (
     <div>
@@ -11,10 +13,52 @@ const Homepage: NextPage = () => {
       </Head>
 
       <main>
-        <h1>Welcome to the playground base template!</h1>
+        <HeaderSection>
+          <Title>Welcome to the playground base template!</Title>
+        </HeaderSection>
+        <BodySection>
+          <Circle />
+          <BodyTitle>Test</BodyTitle>
+        </BodySection>
+        <FooterSection></FooterSection>
       </main>
     </div>
   )
 }
 
 export default Homepage
+
+const HeaderSection = styled('div', {
+  backgroundColor: 'red',
+  height: '100vh',
+})
+
+const BodySection = styled('div', {
+  position: 'relative',
+  backgroundColor: 'White',
+  height: '100vh',
+})
+
+const Circle = styled('div', {
+  position: 'absolute',
+  width: '50px',
+  height: '50px',
+  top: '200px',
+  left: '50%',
+  transform: 'translateX(-50%)',
+  backgroundColor: 'Purple',
+  borderRadius: '50%',
+})
+
+const Title = styled('h1', {
+  color: 'black',
+})
+
+const BodyTitle = styled(Title, {
+  color: 'white',
+})
+
+const FooterSection = styled('div', {
+  backgroundColor: 'Blue',
+  height: '200vh',
+})
